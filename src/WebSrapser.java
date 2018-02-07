ef55eac074412af9a8c77d1c115be1f8e384650d
basmahkishta <bkisht2@illinois.edu>
2018-02-07 12:42:47 -0600

    intial project

--- src/WebSrapser.java
+++ src/WebSrapser.java
@@ -1,6 +1,34 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class WebSrapser {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
 
     public static void main(String[] unused) {
-        System.out.print("Ran");
+        String hamlet = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        int count = 0;
+        String[] text = hamlet.split(" ");
+        for (int i = 0; i < text.length;i++) {
+            if (!text[i].equals(" ")) {
+                count++;
+            }
+        }
+        System.out.print(count);
     }
 }
